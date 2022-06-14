dfb138d914531c31ac3b4433001691aca2cb0240
guilhem <guilhem.cros.38@gmail.com>
2022-06-14 21:45:57 +0200

    modification des constructeur ajout du corp des methode de cast mais bug de detection

--- src/rayCasting/Particle.java
+++ src/rayCasting/Particle.java
@@ -2,9 +2,6 @@ package rayCasting;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-import java.awt.geom.RectangularShape;
-import java.util.ArrayList;
 
 
 /**
@@ -12,31 +9,32 @@ import java.util.ArrayList;
  */
 public class Particle {
 
-    Point initalLocation;
+    Point initalLocation = new Point();
 
-    double direction=0;
+    double angle =0;
 
     int indice=0;
 
-    Point DirectionLocation;
+    double lenght=0;
+
+    Point directionLocation;
 
     public Particle(Point initalLocation){
         this.initalLocation = initalLocation;
     }
 
-    public Particle(Point initalLocation, double direction) {
+    public Particle(Point initalLocation, double angle,double lenght) {
         this.initalLocation = initalLocation;
-        this.direction = direction;
+        this.angle = angle;
+        this.lenght = lenght;
+        this.directionLocation= new Point((int)(Math.cos(angle)*lenght),(int)(Math.sin(angle)*lenght));
     }
 
-    public Particle(Point initalLocation, double direction, int indice) {
+    public Particle(Point initalLocation, double angle,double lenght, int indice) {
         this.initalLocation = initalLocation;
-        this.direction = direction;
+        this.angle = angle;
         this.indice = indice;
-    }
-
-    public Particle(Point initalLocation, Point directionLocation, int indice) {
-        this(initalLocation,Math.atan2(directionLocation.y-initalLocation.y,directionLocation.x-initalLocation.x),indice);
+        this.directionLocation= new Point((int)(Math.cos(angle)*lenght),(int)(Math.sin(angle)*lenght));
     }
     /*
     public Point watch(ArrayList<HitBox2D> hitboxs){
@@ -63,19 +61,73 @@ public class Particle {
 
     /**
      * @param hitbox
-     * @return _event renvoie le CastEvent de l'interaction "il peut être null si il n'y a pas d'interaction"
+     * @return - renvoie le CastEvent de l'interaction - null si il n'y a pas d'interaction
      */
     public CastEvent cast( HitBox2D hitbox){
-        //todo faire le cast de la particule
-        CastEvent event=null;
-        Point interactLocation=null;
+        //fixme ne detecte pas les collision
+        System.out.println("indice: "+indice+", from: ["+ initalLocation.x+", "+initalLocation.y+"], to: ["+ directionLocation.x+", "+directionLocation.y+"]");//todo debug
+
+        double bestDistance=Double.POSITIVE_INFINITY;
+        for(int i = 0;i<hitbox.getCorners().length;i++){
+            double testDistance;
+            if(i==hitbox.getCorners().length-1){
+                testDistance = cast(new Point[]{new Point(hitbox.getCorners()[i]),new Point(hitbox.getCorners()[0])});
+            }else{
+                testDistance = cast(new Point[]{new Point(hitbox.getCorners()[i]),new Point(hitbox.getCorners()[i+1])});
+            }
+            if(bestDistance>testDistance)bestDistance=testDistance;
+        }
+
+        if(!Double.isFinite(bestDistance)){
+            CastEvent event=new CastEvent(initalLocation,new Point((int)(Math.cos(angle)*bestDistance),(int)(Math.cos(angle)*bestDistance)),indice,hitbox);
+        }
+        return null;
+    }
+
+    /**
+     *
+     * @param side - contain the 2 points of a "wall"
+     * @return - return the distance of interaction positve infinity if no interaction
+     *
+     * @see CastEvent
+     */
+    public double cast(Point[] side){
+        double distance;
+
+        double x1 = side[0].x;
+        double y1 = side[0].y;
+        double x2 = side[1].x;
+        double y2 = side[1].y;
+
+        double x3 = this.initalLocation.x;
+        double y3 = this.initalLocation.y;
+        double x4 = this.directionLocation.x;
+        double y4 = this.directionLocation.y;
+
+        double den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+        if (den == 0) {
+            return Double.POSITIVE_INFINITY;
+        }
+
+        double t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
+        double u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den;
+
+        if (t > 0 && t < 1 && u > 0) {
+            distance = Point2D.distance(x3,y3,x1 + t * (x2 - x1),y1 + t * (y2 - y1));
+        } else {
+            return Double.POSITIVE_INFINITY;
+        }
 
 
 
-        event = new CastEvent(initalLocation,interactLocation,indice,hitbox);
 
 
-        return event;
+
+
+
+
+
+        return distance;
     }
 
 
