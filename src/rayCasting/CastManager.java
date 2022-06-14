911cae91b4838cbc7db78e2e7398ad47c67512a9
guilhem <guilhem.cros.38@gmail.com>
2022-06-14 11:28:58 +0200

    création de la classe abstract CastManager

---
+++ src/rayCasting/CastManager.java
@@ -0,0 +1,15 @@
+package rayCasting;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+
+public abstract class CastManager {
+
+
+    public abstract ArrayList<CastEvent> watch(ArrayList<HitBox2D> hitboxs);
+
+
+
+
+}
