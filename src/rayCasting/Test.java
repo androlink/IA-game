9c7675bdbc363acaab3b1c2d508fdbac9ee91e01
guilhem <guilhem.cros.38@gmail.com>
2022-06-15 22:07:31 +0200

    modification des test existant pour l'identification des bug

--- src/rayCasting/Test.java
+++ src/rayCasting/Test.java
@@ -12,9 +12,9 @@ class Test {
     @org.junit.jupiter.api.Test
     public void wallCollideTest(){
         Particle ray = new Particle(new Point(),0,100);
-        Point[] wall1 = {new Point(40,-10),new Point(60,10)};
+        Point[] wall1 = {new Point(50,-10),new Point(50,10)};
         System.out.println(ray.cast(wall1));
-        Point[] wall2 = {new Point(40,10),new Point(60,20)};
+        Point[] wall2 = {new Point(50,10),new Point(50,20)};
         System.out.println(ray.cast(wall2));
 
 
@@ -22,10 +22,11 @@ class Test {
     }
     @org.junit.jupiter.api.Test
     public void hitBoxCollideTest(){
-        HitBox2D hitBox2D = new SimpleSquareHitBox(new Point(0,100),10);
-        Particle ray = new Particle(new Point(),0,1000);
-
-        System.out.println(ray.cast(hitBox2D));
+        HitBox2D hitBox2D = new SimpleSquareHitBox(new Point(0,0),10);
+        Particle ray = new Particle(new Point(),0,100);
+        System.out.println(hitBox2D.toString());
+        System.out.println(ray.toString());
+        System.out.println(ray.cast(hitBox2D).interactLocation);
 
 
     }
@@ -35,7 +36,8 @@ class Test {
 
     @org.junit.jupiter.api.Test
     public void hitBoxManagerCollideTest(){
-        SimpleSquareHitBox test = new SimpleSquareHitBox(new Point(100,100),20);
+        System.out.println(Math.cos(Math.toRadians(0))+" "+Math.sin(Math.toRadians(0)));
+        SimpleSquareHitBox test = new SimpleSquareHitBox(new Point(10,0),2);
         for(int i = 0 ; i < test.getCorners().length;i++)
             System.out.println(test.getCorners()[i]);
 
@@ -48,10 +50,5 @@ class Test {
             System.out.println("indice: "+event.getIndice()+", location: "+event.interactLocation);
         }
     }
-
-
-
-
-
 
 }
