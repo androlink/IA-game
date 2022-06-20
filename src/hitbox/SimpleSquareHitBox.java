8ced3896907eeda2c7b413477c54eb579707da4d
guilhem <guilhem.cros.38@gmail.com>
2022-06-20 10:56:17 +0200

    ajout de methode utile

--- src/hitbox/SimpleSquareHitBox.java
+++ src/hitbox/SimpleSquareHitBox.java
@@ -1,16 +1,21 @@
-package rayCasting;
+package hitbox;
 
 import java.awt.*;
 import java.util.Arrays;
 
-public class SimpleSquareHitBox extends HitBox2D{
+public class SimpleSquareHitBox extends HitBox2D {
     Point[] corners = new Point[4];
+    Side[] sides = new Side[4];
 
-    SimpleSquareHitBox(Point location,int size){
+    public SimpleSquareHitBox(Point location,int size){
         corners[0] = new Point(location.x-size,location.y-size);
         corners[1] = new Point(location.x+size,location.y-size);
         corners[2] = new Point(location.x+size,location.y+size);
         corners[3] = new Point(location.x-size,location.y+size);
+
+        for(int i = 0;i<4;i++) {
+            sides[i]=new Side(new Point(corners[i].x,corners[i].y),new Point(corners[(i+1)%4].x,corners[(i+1)%4].y));
+        }
     }
 
 
@@ -27,9 +32,19 @@ public class SimpleSquareHitBox extends HitBox2D{
     }
 
     @Override
+    public Side[] getSides() {
+        return sides;
+    }
+    @Override
+    public Point[] getSide(int index){
+        return sides[index].getCorner();
+    }
+
+    @Override
     public String toString() {
         return "SimpleSquareHitBox{" +
                 "corners=" + Arrays.toString(corners) +
+                ", sides=" + Arrays.toString(sides) +
                 '}';
     }
 }
