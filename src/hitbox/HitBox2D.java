e2d0f5a62ecf38c21685723c3e42583558c039ab
guilhem <guilhem.cros.38@gmail.com>
2022-06-22 11:41:00 +0200

    ajout de methode de collision

--- src/hitbox/HitBox2D.java
+++ src/hitbox/HitBox2D.java
@@ -1,21 +1,42 @@
 package hitbox;
 
+import rayCasting.CastEvent;
+
 import java.awt.*;
+import java.util.ArrayList;
 
 public abstract class HitBox2D {
 
 
-    /**
-     * @param location
-     * @return  true si le point se trouve dans la hitbox
-     */
-    public abstract boolean contain(Point location);
 
-    public abstract Point[] getCorners();
+    public abstract Point[] getBounds();
 
     public abstract Side[] getSides();
 
     public abstract Point[] getSide(int index);
 
+    public abstract void setLocation(Point location);
+    public abstract void setAngle(double angle);
 
+    public ArrayList<CollisionEvent> collide(HitBox2D testedBox) {
+        ArrayList<CollisionEvent> collisionEvents = new ArrayList<>();
+
+        for(Side mySide:getSides()){
+            for(Side otherSide: testedBox.getSides()){
+                Point pt = mySide.getIntersection(otherSide);
+                if(pt!=null)collisionEvents.add(new CollisionEvent(pt,this,testedBox,mySide,otherSide));
+            }
+        }
+        return collisionEvents;
+    }
+
+
+    public boolean contain(Point location) {
+        return false;
+    }
+    public void draw(Graphics g){
+        for(Side s:getSides()){
+            g.drawLine(s.getCorner()[0].x,s.getCorner()[0].y,s.getCorner()[1].x,s.getCorner()[1].y);
+        }
+    }
 }
--- src/hitbox/Side.java
+++ src/hitbox/Side.java
@@ -17,4 +17,43 @@ public class Side {
     }
 
 
+    public Point getIntersection(Side otherSide){
+
+        double x1 = otherSide.getCorner()[0].x;
+        double y1 = otherSide.getCorner()[0].y;
+        double x2 = otherSide.getCorner()[1].x;
+        double y2 = otherSide.getCorner()[1].y;
+
+        double x3 = getCorner()[0].x;
+        double y3 = getCorner()[0].y;
+        double x4 = getCorner()[1].x;
+        double y4 = getCorner()[1].y;
+
+        double den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+        if (den == 0) {
+            return null;
+        }
+
+        double t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
+        double u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den;
+
+        if (t > 0 && t < 1 && u > 0 && u < 1) {
+            Point pt = new Point();
+            pt.x=(int)(x1 + t * (x2 - x1));
+            pt.y=(int)(y1 + t * (y2 - y1));
+            return pt;
+        } else {
+            return null;
+        }
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Side{" +
+                "p1=" + p1 +
+                ", p2=" + p2 +
+                '}';
+    }
 }
